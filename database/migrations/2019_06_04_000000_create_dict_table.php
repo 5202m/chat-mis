0623138c9241a38450bcc1b91dd6f3f2e8afd5e1
zhuzhenhui <zhuzhenhui@ishunwan.com>
2019-06-04 19:25:46 +0800

    添加创建字典表PHP文件

---
+++ database/migrations/2019_06_04_000000_create_dict_table.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/6/4
+ * Time: 11:46
+ */
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDictTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dict', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('parent_id')->default(0)->comment('父ID');
+            $table->string('code')->unique()->comment('字典编码');
+            $table->string('name_cn')->comment('字典中文名称');
+            $table->string('name_tw')->comment('字典繁体名称');
+            $table->string('name_en')->comment('字典英文名称');
+            $table->smallInteger('sort')->default(0)->comment('排序序列');
+            $table->smallInteger('valid')->default(1)->comment('是否删除：0 、删除 ；1、正常');
+            $table->smallInteger('status')->default(1)->comment('状态：0 、禁用 ；1、启动');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dict');
+    }
+}
